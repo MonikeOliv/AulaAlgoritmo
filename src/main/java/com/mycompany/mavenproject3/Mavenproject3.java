75adf51dac7a2096f891c89d6b4f1a515cb19058
MonikeOliv <monike17gon@gmail.com>
2024-09-27 22:39:11 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject3/Mavenproject3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject3;
+
+/**
+ *
+ * @author Admin
+ */
+public class Mavenproject3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+        System.out.println("i"+i);
+        }while(i!=1);
+    }
+    }
